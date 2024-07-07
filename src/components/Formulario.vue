2fc692fef00ae35084a734be7d0e24aa0aae0043
Kaique <kaiquesimoes58@gmail.com>
2024-07-06 21:23:21 -0300

    alteraões finais

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,29 +1,44 @@
 <script setup>
-const props = defineProps(['getNumero1', 'getNumero2','getOperacao','executaOperacao','retornaResposta']);
+const props = defineProps(['getNumero1', 'getNumero2', 'getOperacao', 'executaOperacao', 'retornaResposta']);
+
+const handleNumero1Change = (event) => {
+    props.getNumero1(event);
+    props.executaOperacao();
+};
+
+const handleNumero2Change = (event) => {
+    props.getNumero2(event);
+    props.executaOperacao();
+};
+
+const handleOperacaoChange = (event) => {
+    props.getOperacao(event);
+    props.executaOperacao();
+};
 </script>
 
 <template>
     <form @submit.prevent="props.executaOperacao">
         <div class="row">
             <div class="col-md-1">
-                <input @change="props.getNumero1" class="form-control " type="number">
+                <input @input="handleNumero1Change" class="form-control" type="number">
             </div>
             <div class="col-md-2">
-                <select @change="props.getOperacao" class="form-control ">
+                <select @input="handleOperacaoChange" class="form-control">
                     <option value="+">Soma (+)</option>
-                    <option value="-">Subitração (-)</option>
+                    <option value="-">Subtração (-)</option>
                     <option value="*">Multiplicação (*)</option>
-                    <option value="/">Divisao (/)</option>
+                    <option value="/">Divisão (/)</option>
                 </select>
             </div>
             <div class="col-md-1">
-                <input @change="props.getNumero2" class="form-control" type="number">
+                <input @input="handleNumero2Change" class="form-control" type="number">
             </div>
             <div class="col-md-1">
                 <button type="submit" class="btn btn-outline-primary">=</button>
             </div>
             <div class="col-1">
-                <input class="resultado" type="text" :value="retornaResposta">
+                <input class="resultado" type="text" :value="props.retornaResposta">
             </div>
         </div>
     </form>
